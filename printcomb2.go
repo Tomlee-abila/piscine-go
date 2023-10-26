4b4c9a3fb433357fdf119bb7731518419317afc5
TABILA <tomleeabila@gmail.com>
2023-10-26 19:26:27 +0300

    c

--- printcomb2.go
+++ printcomb2.go
@@ -15,7 +15,7 @@ func PrintComb2() {
 					z01.PrintRune(k)
 					z01.PrintRune(l)
 
-					if (i = '9' && j == '9' k == '9' && l == '9') {
+					if (i == '9' && j == '9' k == '9' && l == '9') {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
