92afa5809e7284337cc7f25d3379283610968c4b
TABILA <tomleeabila@gmail.com>
2023-10-26 11:42:27 +0300

    please

--- printcomb.go
+++ printcomb.go
@@ -6,17 +6,15 @@ func PrintComb() {
 	z01.Printf("012")
 
 	for i := 13; i <= 999; i++ {
-		
-		firstDigit := i / 100         
-		secondDigit := (i / 10) % 10  
-		thirdDigit := i % 10          
+
+		firstDigit := i / 100
+		secondDigit := (i / 10) % 10
+		thirdDigit := i % 10
 
 		if firstDigit < secondDigit && secondDigit < thirdDigit {
 
 			z01.Printf(", %03d", i)
-		}	
-		
+		}
+
 	}
 }
-
-
