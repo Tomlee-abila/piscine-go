4d3c0ff4bc44eb2542428086c913d90348257b22
TABILA <tomleeabila@gmail.com>
2023-10-26 18:24:33 +0300

    c

--- printcomb.go
+++ printcomb.go
@@ -3,13 +3,14 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for x := 0; x < 8; x++ {
-		for y := x + 1; y < 9; y++ {
-			for k := y + 1; k < 10; k++ {
-				z01.PrintRune(rune('0' + i))
-				z01.PrintRune(rune('0' + y))
-				z01.PrintRune(rune('0' + k))
-				if x != 7 || y != 8 || k != 9 {
+	for i := '0'; i <= '7'; i++ {
+		for j := i + 1; j <= '8'; j++ {
+			for k := j + 1; k <= '9'; k++ {
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(k)
+
+				if i != '7' || j != '8' || k != '9' {
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				}
