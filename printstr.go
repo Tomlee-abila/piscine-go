62dc6460e02ca267c460c1e53d8e942f5884359f
tabila <tomleeabila@gmail.com>
2023-10-27 10:47:56 +0300

    f

--- printstr.go
+++ printstr.go
@@ -8,16 +8,8 @@ func PrintStr(s string) {
 
 	aString := []byte(s)
 
-	// aString := "Hello, World!"
-
 	for i := 0; i < len(aString); i++ {
 		fmt.Printf("%c", aString[i])
 	}
-
-	// for i, _ := range aString {
-	// 	fmt.Printf("%v", aString[i])
-	// }
-
 	fmt.Println()
-
 }
