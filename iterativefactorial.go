92fd7cd25e399bb64e9d7f1129b6c48cddc9258c
tabila <tomleeabila@gmail.com>
2023-10-30 18:14:25 +0300

    iterativeFactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,7 +3,7 @@ package piscine
 func IterativeFactorial(nb int) int {
 	if nb > 0 {
 		result := 1
-		for i := 1; i <= nb; i++ {
+		for i := 1; i < nb+1; i++ {
 			result = result * i
 		}
 		return result
