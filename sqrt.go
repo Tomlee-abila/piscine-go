d9e927a8f336d8e8b79900b9f7bce094e9cbe296
tabila <tomleeabila@gmail.com>
2023-10-31 17:42:05 +0300

    sqr

--- sqrt.go
+++ sqrt.go
@@ -1,22 +1,14 @@
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Println(Sqrt(4))
-	fmt.Println(Sqrt(3))
-}
+package piscine
 
 func Sqrt(nb int) int {
 	sqr := 0
 	for i := 1; i <= nb; i++ {
 		sqr = i * i
 		if sqr == nb {
-			return sqr
+			return i
 		} else if i == nb {
 			return 0
 		}
 	}
+	return 0
 }
