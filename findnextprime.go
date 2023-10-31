77ce9f52227d6d47e3f8fdc9819a767fdbb64b4d
tabila <tomleeabila@gmail.com>
2023-10-31 18:54:27 +0300

    nextPrime

---
+++ findnextprime.go
@@ -0,0 +1,21 @@
+package piscine
+
+func FindNextPrime(nb int) int {
+	for i := nb; i >= nb; i++ {
+		if IsPrime(i) {
+			return i
+		}
+	}
+	return 1
+}
+func IsPrime(nb int) bool {
+	if nb == 1 || nb < 0 || nb == 0 {
+		return false
+	}
+	for i := 1; i < nb; i++ {
+		if i != 1 && nb%i == 0 {
+			return false
+		}
+	}
+	return true
+}
