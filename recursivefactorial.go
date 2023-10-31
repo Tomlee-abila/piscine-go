c4e86c4355104d0218dbf0cd08c5b3988a689ae1
tabila <tomleeabila@gmail.com>
2023-10-31 12:47:21 +0300

    recursive

---
+++ recursivefactorial.go
@@ -0,0 +1,16 @@
+package piscine
+
+func RecursiveFactorial(nb int) int {
+	if nb < 0 {
+		return 0
+	} else if nb == 1 {
+		return 1
+	} else {
+		result := nb * RecursiveFactorial(nb-1)
+		if result < 0 {
+			return 0
+		} else {
+			return result
+		}
+	}
+}
