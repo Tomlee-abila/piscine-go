2792519d5c5dc60cb7a038dd0eec0e48f7708536
tabila <tomleeabila@gmail.com>
2023-10-31 18:05:51 +0300

    prime

---
+++ isprime.go
@@ -0,0 +1,13 @@
+package piscine
+
+func IsPrime(nb int) bool {
+	if nb == 1 || nb < 0 {
+		return false
+	}
+	for i := 1; i < nb; i++ {
+		if i != 1 && nb%i == 0 {
+			return false
+		}
+	}
+	return true
+}
