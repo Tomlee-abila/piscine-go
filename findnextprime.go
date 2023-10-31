45c91081a67bc05671be76193034af09c7fb962c
tabila <tomleeabila@gmail.com>
2023-10-31 19:15:46 +0300

    nextPrime

--- findnextprime.go
+++ findnextprime.go
@@ -1,23 +1,10 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	result := 0
-	for i := nb; i >= nb; i++ {
-		if Prime(i) {
-			result = i
+	for a := nb; a >= nb; a++ {
+		if IsPrime(a) {
+			return a
 		}
 	}
-	return result
-}
-
-func Prime(nb int) bool {
-	if nb == 1 || nb < 0 || nb == 0 {
-		return false
-	}
-	for i := 1; i < nb; i++ {
-		if i != 1 && nb%i == 0 {
-			return false
-		}
-	}
-	return true
+	return 1
 }
