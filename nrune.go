6eeb32828ab20b34db6111be2d88f80a4d38c9f9
tabila <tomleeabila@gmail.com>
2023-11-01 15:24:49 +0300

    nrune

--- nrune.go
+++ nrune.go
@@ -3,7 +3,7 @@ package piscine
 func NRune(s string, n int) rune {
 	number := len(s)
 	sentence := []rune(s)
-	if n > number || n < 0 {
+	if n > number || n < 1 {
 		return 0
 	} else {
 		return sentence[n-1]
