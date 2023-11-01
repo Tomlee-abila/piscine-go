3189b97b98b959c9fd1f820d495263b4f2c9a055
tabila <tomleeabila@gmail.com>
2023-11-01 20:16:02 +0300

    fix

--- toupper.go
+++ toupper.go
@@ -1,11 +1,11 @@
 package piscine
 
 func ToUpper(s string) string {
-	runeS := []rune(s)
+	upper := []rune(s)
 	for i, str := range s {
 		if lower(str) {
-			runeS[i] -= 32
+			upper[i] -= 32
 		}
 	}
-	return string(runeS)
+	return string(upper)
 }
