83543f21e63694b3721a0ab91602169cc8852bfa
tabila <tomleeabila@gmail.com>
2023-11-03 02:57:55 +0300

    please

--- capitalize.go
+++ capitalize.go
@@ -37,20 +37,3 @@ func toUpper(r rune) rune {
 	}
 	return r
 }
-
-
-package main
-
-import (
-	"github.com/01-edu/z01"
-
-	"os"
-)
-
-func main() {
-	title := []rune(os.Args[0])
-	for i := 2; i < len(title); i++ {
-		z01.PrintRune(title[i])
-	}
-	z01.PrintRune('\n')
-}
