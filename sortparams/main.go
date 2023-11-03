54e6905a9312005560662a02bb4aa8b32c9b2993
tabila <tomleeabila@gmail.com>
2023-11-03 04:18:13 +0300

    please

--- sortparams/main.go
+++ sortparams/main.go
@@ -6,21 +6,24 @@ import (
 	"github.com/01-edu/z01"
 )
 
+func insertionSort(args []string) {
+	for i := 1; i < len(args); i++ {
+		j := i
+		for j > 0 && args[j] < args[j-1] {
+			args[j], args[j-1] = args[j-1], args[j]
+			j--
+		}
+	}
+}
+
 func main() {
-	for i := 1; i < len(os.Args); i++ {
-		title := []rune(os.Args[i])
-		for i := 0; i < len(title); i++ {
-			z01.PrintRune(title[i])
+	args := os.Args[1:]
+	insertionSort(args)
+
+	for _, arg := range args {
+		for _, ch := range arg {
+			z01.PrintRune(ch)
 		}
 		z01.PrintRune('\n')
 	}
-	// for j := 0; j < len(os.Args)-1; j++ {
-	// 	for i := 1; i < len(os.Args)-1; j++ {
-	// 		if os.Args[i] > os.Args[i+1] {
-	// 			minValue := os.Args[i]
-	// 			os.Args[i] = os.Args[i+1]
-	// 			os.Args[i+1] = minValue
-	// 		}
-	// 	}
-	// }
 }
