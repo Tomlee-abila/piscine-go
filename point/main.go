aef7f331834b72399b551a061082934775925ad7
tabila <tomleeabila@gmail.com>
2023-11-07 18:15:43 +0300

    display

--- point/main.go
+++ point/main.go
@@ -9,27 +9,6 @@ type point struct {
 	y int
 }
 
-func printStr(s string) {
-	for _, r := range s {
-		z01.PrintRune(r)
-	}
-}
-
-func printInt(i int) {
-	r := '0'
-	if i == 1 {
-		r += +1
-	} else if i == 2 {
-		r += 2
-	} else {
-		r += 4
-	}
-	// for j := 0; j < i; j++ {
-	// 	r++
-	// }
-	z01.PrintRune(r)
-}
-
 func setPoint(ptr *point) {
 	ptr.x = 42
 	ptr.y = 21
@@ -37,9 +16,9 @@ func setPoint(ptr *point) {
 
 func main() {
 	points := &point{}
+	setPoint(points)
 	s1 := "x = "
 	s2 := ", y = "
-	setPoint(points)
 	printStr(s1)
 	printInt((points.x / 10))
 	printInt((points.x % 10))
@@ -48,4 +27,17 @@ func main() {
 	printInt((points.y % 10))
 	z01.PrintRune('\n')
 	// vziPusU.m#cc4Bi
+}
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+func printInt(i int) {
+	r := '0'
+	for j := 0; j < i; j++ {
+		r++
+	}
+	z01.PrintRune(r)
 }
