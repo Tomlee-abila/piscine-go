bb2aaa84eceaaeff8511bba99b7ba0e2a7ad3c49
tabila <tomleeabila@gmail.com>
2023-11-08 16:44:03 +0300

    issorted

--- issorted.go
+++ issorted.go
@@ -1,10 +1,22 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
+	x := 0
+	y := 0
 	for i := 0; i < (len(a) - 1); i++ {
-		if !((f(a[i], a[i+1])) > 0) {
-			return false
+		if !((f(a[i], a[i+1])) >= 0) {
+			x = 1
 		}
 	}
-	return true
+
+	for i := 0; i < (len(a) - 1); i++ {
+		if !((f(a[i], a[i+1])) <= 0) {
+			y = 1
+		}
+	}
+
+	if x == 0 || y == 0 {
+		return true
+	}
+	return false
 }
