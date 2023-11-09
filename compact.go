19045c2bce3de394593259a74aa64ff8a5bd3fbd
tabila <tomleeabila@gmail.com>
2023-11-09 15:00:28 +0300

    s

--- compact.go
+++ compact.go
@@ -2,11 +2,14 @@ package piscine
 
 func Compact(ptr *[]string) int {
 	arr := *ptr
+	var arr1 []string
 	count := 0
 	for _, s := range arr {
-		if s == "" {
+		if !(s == "") {
 			count++
+			arr1 = append(arr1, s)
 		}
 	}
+	*ptr = arr1
 	return count
 }
