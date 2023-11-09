7b53882eda159b57b6d9702453e6fbfd4195731e
tabila <tomleeabila@gmail.com>
2023-11-09 11:37:46 +0300

    foodDelivery

--- fooddeliverytime.go
+++ fooddeliverytime.go
@@ -17,6 +17,9 @@ func FoodDeliveryTime(order string) int {
 	setPoint(preptime)
 	runeOrder := []rune(order)
 	totalTime := 0
+	ch := 0
+	br := 0
+	nu := 0
 
 	for i, ch := range runeOrder {
 		if ch == 'b' {
@@ -25,6 +28,7 @@ func FoodDeliveryTime(order string) int {
 					return 404
 				}
 			}
+			br++
 			totalTime += preptime.burger
 		} else if ch == 'c' {
 			for j, rn := range "chips" {
@@ -32,6 +36,7 @@ func FoodDeliveryTime(order string) int {
 					return 404
 				}
 			}
+			ch++
 			totalTime += preptime.chips
 		} else if ch == 'n' {
 			for j, rn := range "nuggets" {
@@ -39,8 +44,12 @@ func FoodDeliveryTime(order string) int {
 					return 404
 				}
 			}
+			nu++
 			totalTime += preptime.nuggets
 		}
+	}
+	if (ch * len("chips"))+(br * len("burger"))+(nu * len("nuggets")) != len(runeOrder){
+		return 404
 	}
 	return totalTime
 }
