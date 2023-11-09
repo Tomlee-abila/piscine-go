706f1ef2c9b525037c4c5c99a5818ea521890d00
tabila <tomleeabila@gmail.com>
2023-11-09 17:11:55 +0300

    shopping

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,19 +1,26 @@
 package piscine
 
-func ShoppingSummaryCounter(s string) []string {
+func ShoppingSummaryCounter(str string) map[string]int {
+	summary := make(map[string]int)
+
+	words := splitWords(str)
+	for _, word := range words {
+		summary[word]++
+	}
+	return summary
+}
+
+func splitWords(s string) []string {
 	word := ""
 	var result []string
 	firstWord := true
-
 	for i, chr := range s {
 		if chr >= 'a' && chr <= 'z' || chr >= 'A' && chr <= 'Z' {
 			word += string(chr)
 		}
-
 		if chr == ' ' || i == len(s)-1 {
 			firstWord = false
 		}
-
 		if firstWord == false && word != "" {
 			firstWord = true
 			result = append(result, word)
