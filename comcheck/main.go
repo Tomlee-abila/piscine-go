4293a498b8dca6f428f436ca2399b5ad502ce0f4
tabila <tomleeabila@gmail.com>
2023-11-09 23:30:19 +0300

    please

--- comcheck/main.go
+++ comcheck/main.go
@@ -6,34 +6,14 @@ import (
 )
 
 func main() {
-	alert := false
-	if len(os.Args) > 1 {
-		for i := 1; i < len(os.Args); i++ {
-			if compare(os.Args[i]) {
-				alert = true
-			}
+	args := os.Args[1:]
+	isTrue := false
+	for i := 0; i < len(args); i++ {
+		if args[i] == "01" || args[i] == "galaxy" || args[i] == "galaxy 01" {
+			isTrue = true
 		}
 	}
-	if alert {
+	if isTrue {
 		fmt.Println("Alert!!!")
 	}
-}
-
-func compare(s string) bool {
-	if len(s) > 1 {
-
-		arr := []rune(s)
-		for i := 0; i < len(arr); i++ {
-			if arr[i] == '0' {
-				if arr[i] == '0' && arr[i+1] == '1' {
-					return true
-				}
-			} else if arr[i] == 'g' {
-				if arr[i] == 'g' && arr[i+1] == 'a' && arr[i+2] == 'l' && arr[i+3] == 'a' && arr[i+4] == 'x' && arr[i+5] == 'y' {
-					return true
-				}
-			}
-		}
-	}
-	return false
 }
