25313ea414e0f9431712ec87ed8e47b9886ca81f
tabila <tomleeabila@gmail.com>
2023-11-09 03:46:39 +0300

    cat

--- cat/main.go
+++ cat/main.go
@@ -1,26 +1,54 @@
 package main
 
 import (
+	"os"
+
 	"fmt"
 	"io/ioutil"
-	"os"
 )
 
 func main() {
-	if len(os.Args) > 2 {
-		fmt.Println("Too many arguments")
+	if len(os.Args) > 1 {
+		for i := 1; i < len(os.Args); i++ {
+			if openFile(os.Args[i]) {
+				readFile(os.Args[i])
+			}
+		}
 		return
 	} else if len(os.Args) < 2 {
-		fmt.Println("File name missing")
-		return
+		for i := 0; i < 10; i++ {
+			fmt.Println("Hello")
+			i = 0
+		}
 	}
-	filename := os.Args[1]
-	// Read the content of the file
-	content, err := ioutil.ReadFile(filename)
+}
+
+func openFile(fName string) bool {
+
+	file, err := os.Open(fName)
 	if err != nil {
-		fmt.Println("Error reading file:", err)
+		fmt.Println("ERROR:", err.Error())
+		return false
+	}
+	defer file.Close()
+	return true
+}
+
+func readFile(filename string) {
+	content, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println(err.Error())
 		return
 	}
-	// Print the content of the file as a string
 	fmt.Print(string(content))
+	// filename := os.Args[1]
+	// // Read the content of the file
+	// content, err := ioutil.ReadFile(filename)
+	// if err != nil {
+	// 	fmt.Println("Error reading file:", err)
+	// 	return
+	// }
+	// // Print the content of the file as a string
+	// fmt.Print(string(content))
 }
