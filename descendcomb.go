1b69cf9d0d7e21bb415e0102f4dd9a8ffd30cae2
tabila <tomleeabila@gmail.com>
2023-11-09 16:25:29 +0300

    desce

--- descendcomb.go
+++ descendcomb.go
@@ -1,22 +1,32 @@
-package piscine
+package main
 
 import "github.com/01-edu/z01"
+
+func main() {
+	DescendComb()
+}
+
+// package piscine
 
 func DescendComb() {
 	for i := '9'; i >= '0'; i-- {
 		for j := '9'; j >= '0'; j-- {
 			for k := '9'; k >= '0'; k-- {
 				for l := '9'; l >= '0'; l-- {
-					if i >= j {
+					if (int(i)*10)+int(j) > (int(k)*10)+int(l) {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
-					}
-					if k >= l {
+						z01.PrintRune(' ')
+						// }
+						// if k >= l {
 						z01.PrintRune(k)
 						z01.PrintRune(l)
+						z01.PrintRune(',')
+						// z01.PrintRune('\n')
 					}
 				}
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
