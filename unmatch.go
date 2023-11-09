66e01d5b6f358a77fc98445e072b3c5a920eee73
tabila <tomleeabila@gmail.com>
2023-11-09 10:10:57 +0300

    uma

---
+++ unmatch.go
@@ -0,0 +1,23 @@
+package piscine
+
+func Unmatch(a []int) int {
+	Double := false
+	count := 0
+	var result int
+	for i, n := range a {
+		Double = false
+		count = 0
+		for j, m := range a {
+			count++
+			if i != j {
+				if n == m {
+					Double = true
+				}
+			}
+			if count == len(a) && Double == false {
+				result = m
+			}
+		}
+	}
+	return result
+}
