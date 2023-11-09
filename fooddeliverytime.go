e646a1246587095a4d4fcba3a5d2c8728faf5485
tabila <tomleeabila@gmail.com>
2023-11-09 12:07:58 +0300

    s

--- fooddeliverytime.go
+++ fooddeliverytime.go
@@ -1,17 +1,4 @@
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Println(FoodDeliveryTime("burger"))
-	fmt.Println(FoodDeliveryTime("chips"))
-	fmt.Println(FoodDeliveryTime("nuggets"))
-	fmt.Println(FoodDeliveryTime("burger") + FoodDeliveryTime("chips") + FoodDeliveryTime("nuggets"))
-}
-
-// package piscine
+package piscine
 
 type food struct {
 	burger  int
