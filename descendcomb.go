08c3e53c96ad86e428877da3b450f204d5026353
tabila <tomleeabila@gmail.com>
2023-11-09 14:51:11 +0300

    com

--- descendcomb.go
+++ descendcomb.go
@@ -1,14 +1,11 @@
-// package main
+package piscine
 
-// func main() {
-// 	DescendComb()
-// }
 
-// func DescendComb() {
-// 	for i := 9; i >= 0; i-- {
-// 		for j := 9; j >= 0; j-- {
+func DescendComb() {
+	for i := 9; i >= 0; i-- {
+		for j := 9; j >= 0; j-- {
 
-// 		}
-// 	}
+		}
+	}
 
-// }
+}
