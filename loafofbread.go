a900eb9362f24e4ae5ff7166fd30576d8448966c
tabila <tomleeabila@gmail.com>
2023-11-10 07:09:04 +0300

    loaf

--- loafofbread.go
+++ loafofbread.go
@@ -3,21 +3,21 @@ package piscine
 func LoafOfBread(str string) string {
 	result := ""
 	count := 0
-	if len(str) >= 5 {
-		for _, ch := range str {
-			if count == 5 {
-				result += string(' ')
-				count = 0
-			} else {
-				if !(ch == ' ') {
-					count++
-					result += string(ch)
-				}
+	if len(str) <= 5 && str != "" {
+		result += "Invalid Output\n"
+		return result
+	}
+	for _, ch := range str {
+		if count == 5 {
+			result += string(' ')
+			count = 0
+		} else {
+			if !(ch == ' ') {
+				count++
+				result += string(ch)
 			}
 		}
-		result += string('\n')
-	} else {
-		result += "Invalid Output\n"
 	}
+	result += string('\n')
 	return result
 }
