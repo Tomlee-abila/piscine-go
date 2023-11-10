2f310ef5983760d171c99f54640c8795b09e252d
tabila <tomleeabila@gmail.com>
2023-11-10 07:04:24 +0300

    loaf

--- loafofbread.go
+++ loafofbread.go
@@ -5,11 +5,11 @@ func LoafOfBread(str string) string {
 	count := 0
 	if len(str) >= 5 {
 		for _, ch := range str {
-			if !(ch == ' ') {
-				if count == 5 {
-					result += string(' ')
-					count = 0
-				} else {
+			if count == 5 {
+				result += string(' ')
+				count = 0
+			} else {
+				if !(ch == ' ') {
 					count++
 					result += string(ch)
 				}
