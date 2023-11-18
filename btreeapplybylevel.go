314d1d5df096586806e71c4ce382091b75fed08d
tabila <tomleeabila@gmail.com>
2023-11-18 20:33:02 +0300

    wah

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,15 +1,19 @@
 package piscine
+
 // package main
 // import (
-// 	"fmt"
+//
+//	"fmt"
+//
 // )
-// func main() {
-// 	root := &TreeNode{Data: "4"}
-// 	BTreeInsertData(root, "1")
-// 	BTreeInsertData(root, "7")
-// 	BTreeInsertData(root, "5")
-// 	BTreeApplyByLevel(root, fmt.Println)
-// }
+//
+//	func main() {
+//		root := &TreeNode{Data: "4"}
+//		BTreeInsertData(root, "1")
+//		BTreeInsertData(root, "7")
+//		BTreeInsertData(root, "5")
+//		BTreeApplyByLevel(root, fmt.Println)
+//	}
 func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
