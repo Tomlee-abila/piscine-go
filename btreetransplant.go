12ea93a93f0f136afe8ada4315487709a0bda533
tabila <tomleeabila@gmail.com>
2023-11-18 21:58:26 +0300

    plant

--- btreetransplant.go
+++ btreetransplant.go
@@ -70,17 +70,26 @@ package piscine
 // }
 
 func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
-	if root.Data == node.Data {
-		root.Data = rplc.Data
+	// If the node to be replaced is the root of the tree
+	if node.Parent == nil {
+		root = rplc
+	} else if node == node.Parent.Left {
+		// If the node is the left child of its parent
+		node.Parent.Left = rplc
+	} else {
+		// If the node is the right child of its parent
+		node.Parent.Right = rplc
 	}
 
-	if root.Data > node.Data {
-		BTreeTransplant(root.Left, node, rplc)
+	// If the replacement node is not nil, update its parent
+	if rplc != nil {
+		rplc.Parent = node.Parent
 	}
 
-	if root.Data < node.Data {
-		BTreeTransplant(root.Right, node, rplc)
-	}
+	// Disconnect the replaced node from the tree
+	node.Parent = nil
+	node.Left = nil
+	node.Right = nil
 
 	return root
 }
