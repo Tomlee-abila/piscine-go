f0aabf3ffc64550f94d656a3f234952a9b9953d6
tabila <tomleeabila@gmail.com>
2023-11-18 21:04:23 +0300

    minimum

---
+++ btreemin.go
@@ -0,0 +1,56 @@
+package piscine
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	root := &TreeNode{Data: "4"}
+// 	BTreeInsertData(root, "1")
+// 	BTreeInsertData(root, "7")
+// 	BTreeInsertData(root, "5")
+// 	min := BTreeMin(root)
+// 	fmt.Println(min.Data)
+// }
+
+// type TreeNode struct {
+// 	Left, Right, Parent *TreeNode
+// 	Data                string
+// }
+
+// func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+// 	newNode := &TreeNode{Data: data}
+
+// 	if root == nil {
+// 		return newNode
+// 	}
+
+// 	if data > root.Data {
+// 		root.Right = BTreeInsertData(root.Right, data)
+// 		root.Right.Parent = root
+// 	}
+
+// 	if data < root.Data {
+// 		root.Left = BTreeInsertData(root.Left, data)
+// 		root.Left.Parent = root
+// 	}
+
+// 	return root
+// }
+
+func BTreeMin(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	node := root
+	result := root
+
+	// node = BTreeMax(root.Right)
+	for node != nil {
+		result = node
+		node = node.Left
+	}
+	return result
+}
