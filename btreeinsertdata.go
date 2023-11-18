ab3ab6a15a3741192950557fc42badb298bed6cf
tabila <tomleeabila@gmail.com>
2023-11-18 16:38:56 +0300

    apply in order

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -31,12 +31,10 @@ func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 
 	if data > root.Data {
 		root.Right = BTreeInsertData(root.Right, data)
-		root.Right.Parent = root
 	}
 
 	if data < root.Data {
 		root.Left = BTreeInsertData(root.Left, data)
-		root.Left.Parent = root
 	}
 
 	return root
