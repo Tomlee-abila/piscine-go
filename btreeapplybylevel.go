e0984755bdc89c8b41fb145933f590425bcd7d6d
tabila <tomleeabila@gmail.com>
2023-11-18 19:05:08 +0300

    btreeapplybylevel.go

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,16 +1,25 @@
-package main
+package piscine
 
-import (
-	"fmt"
-)
+// package main
 
-func main() {
-	root := &TreeNode{Data: "4"}
-	BTreeInsertData(root, "1")
-	BTreeInsertData(root, "7")
-	BTreeInsertData(root, "5")
-	BTreeApplyByLevel(root, fmt.Println)
-}
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	root := &TreeNode{Data: "4"}
+// 	BTreeInsertData(root, "1")
+// 	BTreeInsertData(root, "7")
+// 	BTreeInsertData(root, "5")
+// 	BTreeApplyByLevel(root, fmt.Println)
+// }
 
 func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+
+	f(root.Data)
+	BTreeApplyByLevel(root.Left, f)
+	BTreeApplyByLevel(root.Right, f)
 }
