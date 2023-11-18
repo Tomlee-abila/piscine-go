786b2ec7e6624d646741a55e36a0b257a136e3bf
tabila <tomleeabila@gmail.com>
2023-11-18 16:20:30 +0300

    insert

---
+++ btreeinsertdata.go
@@ -0,0 +1,43 @@
+package piscine
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	root := &TreeNode{Data: "4"}
+// 	BTreeInsertData(root, "1")
+// 	BTreeInsertData(root, "7")
+// 	BTreeInsertData(root, "5")
+// 	fmt.Println(root.Left.Data)
+// 	fmt.Println(root.Data)
+// 	fmt.Println(root.Right.Left.Data)
+// 	fmt.Println(root.Right.Data)
+// }
+
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	newNode := &TreeNode{Data: data}
+
+	if root == nil {
+		return newNode
+	}
+
+	if data > root.Data {
+		root.Right = BTreeInsertData(root.Right, data)
+		root.Right.Parent = root
+	}
+
+	if data < root.Data {
+		root.Left = BTreeInsertData(root.Left, data)
+		root.Left.Parent = root
+	}
+
+	return root
+}
